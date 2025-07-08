6ab40525925053ffc2512dbd5701ae7396f2bb20
Devotedjay <129480890+Devotedjay@users.noreply.github.com>
2025-07-08 15:55:56 +0700

    Update daily.py

--- daily.py
+++ daily.py
@@ -1,9 +1,11 @@
+import os
 import requests
 import datetime
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1392061849813516369/9hfWDo5jghXYODrE2EL6czl9NmdiDC6zH-pA8II4tyJ-3lcMoGh9NkgpCFpt3y1Ncrj-"
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 TARGET_DATE = datetime.date(2026, 6, 1)
 today = datetime.date.today()
 delta = (TARGET_DATE - today).days
 
-requests.post(WEBHOOK_URL, json={"content": f"📅 Còn {delta} ngày nữa đến ngày 01/06/2026!"})
+message = f"📅 Còn {delta} ngày nữa đến ngày 01/06/2026!"
+requests.post(WEBHOOK_URL, json={"content": message})
