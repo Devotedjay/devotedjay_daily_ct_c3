6532875ec0c8ee3419a253dc461c257e040d0eec
Devotedjay <129480890+Devotedjay@users.noreply.github.com>
2025-07-08 15:37:47 +0700

    Update daily.py

--- daily.py
+++ daily.py
@@ -1,7 +1,7 @@
 import requests
 import datetime
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1392052960384778300/1TPnZkvq_KYytLoJLdcPV3fDIMXBLgRalQUCBTmyp6Q_022aiPDBheNQVVc79_xsfwku"
+WEBHOOK_URL = "https://discord.com/api/webhooks/1392061849813516369/9hfWDo5jghXYODrE2EL6czl9NmdiDC6zH-pA8II4tyJ-3lcMoGh9NkgpCFpt3y1Ncrj-"
 TARGET_DATE = datetime.date(2026, 6, 1)
 today = datetime.date.today()
 delta = (TARGET_DATE - today).days
